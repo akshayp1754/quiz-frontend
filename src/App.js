4043804d896e69412466da2207f6677276394f0c
akshayp1754 <akshayp1754@gmail.com>
2023-07-31 21:37:19 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Category from "./Category";
+import './App.css'
+import Comprehension from "./Comprehension";
+import Form from "./Form";
+
+function App() {
+  return (
+    <div>
+      <Category></Category>
+      {/* <Comprehension></Comprehension> */}
+      <Form></Form>
+    </div>
+  );
+}
+
+export default App;
